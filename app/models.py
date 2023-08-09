e475db3e510fd149358a5af675e881aef291b8f8
Dmitrii Sergeichuk <d.sergeichuk@ivideon.com>
2023-08-09 23:25:59 +0200

    fix: create proper relationship models

--- app/models.py
+++ app/models.py
@@ -1,40 +1,86 @@
-from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import backref, relationship
+# How to make relationships: https://docs.sqlalchemy.org/en/20/orm/basic_relationships.html
 
-Base = declarative_base()
+from typing import List
 
-dish_ingredient = Table(
-    'dish_ingredient',
-    Base.metadata,
-    Column('dish_id', Integer, ForeignKey('dish.id')),
-    Column('ingredient_id', Integer, ForeignKey('ingredient.id')),
-    Column('amount', Float)
-    )
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, Session
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class DishIngredient(Base):
+    __tablename__ = 'dish_ingredient'
+
+    dish_id: Mapped[int] = mapped_column(ForeignKey('dish.id'), primary_key=True)
+    ingredient_id: Mapped[int] = mapped_column(ForeignKey('ingredient.id'), primary_key=True)
+
+    amount: Mapped[float]
+
+    dish: Mapped['Dish'] = relationship(back_populates='ingredients')
+    ingredient: Mapped['Ingredient'] = relationship(back_populates='dishes')
+
+    def __repr__(self) -> str:
+        return str(vars(self))
+
 
 class Unit(Base):
     __tablename__ = 'unit'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    
-    def __repr__(self):
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+
+    def __repr__(self) -> str:
         return str(vars(self))
+
 
 class Ingredient(Base):
     __tablename__ = 'ingredient'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    unit_id = Column(Integer, ForeignKey('unit.id'))
-    dishes = relationship('Dish', secondary=dish_ingredient, back_populates='ingredients')
-    
-    def __repr__(self):
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    unit_id: Mapped[int] = mapped_column(ForeignKey('unit.id'))
+    name: Mapped[str] = mapped_column()
+    unit: Mapped[Unit] = relationship()
+    dishes: Mapped[List[DishIngredient]] = relationship(back_populates='ingredient')
+
+    def __repr__(self) -> str:
         return str(vars(self))
-    
+
+
 class Dish(Base):
     __tablename__ = 'dish'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    ingredients = relationship('Ingredient', secondary=dish_ingredient, back_populates='dishes')
-    
-    def __repr__(self):
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    ingredients: Mapped[List[DishIngredient]] = relationship(back_populates='dish')
+
+    def __repr__(self) -> str:
         return str(vars(self))
+
+
+if __name__ == '__main__':
+    engine = create_engine("sqlite:///tmp.db")
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    # create unit
+    gramm = Unit(name='gramm')
+    item = Unit(name='item')
+
+    session.add_all([gramm, item])
+    session.commit()
+
+    # create dish
+    potato = Ingredient(name='potato', unit=item)
+    salt = Ingredient(name='salt', unit=gramm)
+
+    ponfrit = Dish(name='ponfrit')
+    ponfrit.ingredients.append(DishIngredient(amount=12.43, ingredient=potato))
+    ponfrit.ingredients.append(DishIngredient(amount=1.2, ingredient=salt))
+
+    session.add(ponfrit)
+    session.commit()
+
+    print()
